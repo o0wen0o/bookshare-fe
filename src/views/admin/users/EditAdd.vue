72dcbb6341c0a4534328e77fc2e2ca805bb2acbd
wenxuan <wen200202@hotmail.com>
2024-03-12 14:51:01 +0800

    Update user edit-add, read page

--- src/views/admin/users/EditAdd.vue
+++ src/views/admin/users/EditAdd.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <span>{{ isEdit ? 'Edit' : 'Add' }} Page</span>
+  <div>
+    <div class="page_header">
+      <h1 class="page_title">{{ isEdit ? "Edit" : "Add" }} User</h1>
     </div>
+    
+    <v-card class="page_body" elevation="3" style="height: 500px; width: 100%">
+      
+    </v-card>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 const isEdit = ref(false);
@@ -20,3 +26,19 @@ onMounted(() => {
   isEdit.value = Boolean(id.value);
 });
 </script>
+
+<style scoped>
+.page_header {
+    padding: 20px 0 40px 20px;
+    display: flex;
+    align-items: center;
+}
+
+.page_title {
+    margin: 0 20px 0 50px;
+}
+
+.page_body {
+    padding: 20px;
+}
+</style>
--- src/views/admin/users/Read.vue
+++ src/views/admin/users/Read.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        <span>Read Page</span>
+  <div>
+    <div class="page_header">
+      <h1 class="page_title">View User</h1>
     </div>
+    
+    <v-card class="page_body" elevation="3" style="height: 500px; width: 100%">
+      
+    </v-card>
+  </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.page_header {
+    padding: 20px 0 40px 20px;
+    display: flex;
+    align-items: center;
+}
+
+.page_title {
+    margin: 0 20px 0 50px;
+}
+
+.page_body {
+    padding: 20px;
+}
+</style>
